5ca0e1e610f67ed858fa2664ce2d74b9e13469fe
Frederik <seumalp@gmail.com>
2021-02-09 12:46:48 +0100

    create dialog barebone

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct Line
+{
+	[TextArea(2, 5)] public string text;
+}
+
+[CreateAssetMenu(fileName = "New Dialog", menuName = "Dialog")]
+public class Dialog : ScriptableObject
+{
+	public Line[] lines;
+}
