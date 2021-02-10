c8f340be6619a270c2289f5f609f4c32d0cf6fe4
Frederik <seumalp@gmail.com>
2021-02-10 13:16:42 +0100

    #QC-12 Added first test cases for dialog iteration and added NSubsitute for later purposes

---
+++ Assets/Tests/DialogTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class DialogTest
+    {
+        private Dialog _dialog;
+        
+        [Test]
+        public void Dialog_advanced_one_time()
+        {
+            // ACT
+            CreateDialog();
+
+            int index = 0;
+            
+            // ASSERT
+            DiaManager.AdvanceLine(_dialog, out string outline, index, out index);
+            
+            Assert.AreEqual(1, index);
+            Assert.AreEqual("Lorem Ipsum", outline);
+        }
+
+        [Test]
+        public void Dialog_advance_two_times()
+        {
+            // ACT
+            CreateDialog();
+            int index = 0;
+            
+            
+            // ASSERT
+            DiaManager.AdvanceLine(_dialog, out string outline, index, out index);
+            Assert.AreEqual(1, index);
+            Assert.AreEqual("Lorem Ipsum", outline);
+            
+            DiaManager.AdvanceLine(_dialog, out outline, index, out index);
+            Assert.AreEqual(2, index);
+            Assert.AreEqual("Test", outline);
+        }
+
+        private void CreateDialog()
+        {
+            // Create the Dialog for the test
+            _dialog = ScriptableObject.CreateInstance<Dialog>();
+            List<Line> lines = new List<Line>();
+            string[] inLine = { "Lorem Ipsum", "Test", "Lorem Ipsum 2" };
+            foreach (string s in inLine)
+            {
+                Line line = new Line();
+                line.text = s;
+                lines.Add(line);
+            }
+
+            _dialog.lines = lines.ToArray();
+        }
+    }
+}
